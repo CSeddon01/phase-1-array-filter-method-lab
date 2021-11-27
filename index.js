f1264b0d40618f464b8df428e85306d3368bd0b0
Christine Seddon <noddes@gmail.com>
2021-11-27 13:59:56 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching (driversArray, name) {
+    return driversArray.filter(function (driver) {
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, ab) {
+    let fuzzyDriver = drivers.filter(function(driver){
+        if (driver.substring(0, 2) === ab )
+            return driver
+    })
+    return fuzzyDriver
+}
+
+
+
+function matchName (driversArray, name) {
+    return driversArray.filter( function (driver) {
+        if (driver.name === name) {
+            return driver
+        }
+    })
+} 
